71d0c1880e5cd5e1f845a5658788d735d9edcb85
Bernhard Leder <bernhard.leder@student.tugraz.at>
2021-12-22 13:29:17 +0100

    added and reorganized tests, changed weird prop recalculation for cellwise proportion

--- src/test/java/org/apache/sysds/test/functions/builtin/part2/BuiltinAmputeTest.java
+++ src/test/java/org/apache/sysds/test/functions/builtin/part2/BuiltinAmputeTest.java
@@ -21,14 +21,11 @@ package org.apache.sysds.test.functions.builtin.part2;
 
 import org.apache.sysds.common.Types.ExecMode;
 
-import java.util.Arrays;
 
 import org.apache.commons.lang.ArrayUtils;
-import org.apache.commons.math3.linear.MatrixUtils;
-import org.apache.hadoop.yarn.webapp.hamlet.HamletSpec.I;
 import org.apache.sysds.runtime.lineage.LineageCacheConfig.ReuseCacheType;
+import org.apache.sysds.runtime.matrix.data.MatrixValue.CellIndex;
 import org.apache.sysds.runtime.meta.MatrixCharacteristics;
-import org.apache.sysds.runtime.meta.MetaData;
 import org.apache.sysds.test.AutomatedTestBase;
 import org.apache.sysds.test.TestConfiguration;
 import org.junit.Assert;
@@ -58,20 +55,24 @@ public class BuiltinAmputeTest extends AutomatedTestBase {
 	private static final String TEST_CLASS_DIR = TEST_DIR + BuiltinAmputeTest.class.getSimpleName() + "/";
 
     private final static int numSamples = 10000;
-	private final static int numFeatures = 14;
+	private final static int numFeatures = 11;
 
     private final double[][] data = getRandomMatrix(numSamples, numFeatures, 1, 10, 1.0, 42);
-    private final double prop = 0.25;
+    private final double defaultProp = 0.5;
     private final double[][] patterns = getPatterns(false);
     private final double[][] frequencies = getFrequencies(true);
     private final double[][] weights = getRandomMatrix(patterns.length, numFeatures, -1.0, 1.0, 0.9, 42);
+    private final double[][] type = getTypes();
     
 
     private double[][] getPatterns(boolean rowOfOnes) {
+        // number of patterns = number of features for this one
+        // simple pattern using zeros on diagonals (like an X) for amputation
+        // param rowOfOnes was used for testing an edge case where patterns without zeros exist
         double [][] tmpPatterns = new double[numFeatures][numFeatures];
         for(int i = 0; i < numFeatures; i++) {
             for (int j = 0; j < numFeatures; j++) {
-                if(i != j) tmpPatterns[i][j] = 1.0;
+                if(i != j && i+j != numFeatures - 1) tmpPatterns[i][j] = 1.0;
                 if (rowOfOnes && i == 0) {
                     tmpPatterns[i][j] = 1.0;
                 }
@@ -108,6 +109,30 @@ public class BuiltinAmputeTest extends AutomatedTestBase {
         }
     }
 
+    private double[][] getTypes() {
+        double[] allowedTypes = {-2.0, 1.0, 0.0, 1.0};
+        double [][] tmpTypes = new double[patterns.length][1];
+        int counter = 0;
+        for (int row = 0; row < tmpTypes.length; row++) {
+            for (int col = 0; col < tmpTypes[row].length; col++) {
+                tmpTypes[row][col] = allowedTypes[counter % allowedTypes.length]; 
+                counter++;
+            }
+        }
+        return tmpTypes;
+
+    }
+
+    private boolean proportionTest(Double expectedProp, Double sampleProp) {
+        // two sided prop test using alpha = 0.05
+        // H0: pi = prop
+        // H1: pi != prop
+        double z_boundary = 1.96;
+        double z = (sampleProp - expectedProp) / Math.sqrt(expectedProp*(1-expectedProp)/ numSamples);
+        return z < z_boundary;
+
+    }
+
     @Override
 	public void setUp() {
         TestConfiguration tc = new TestConfiguration(TEST_CLASS_DIR, TEST_NAME, new String[]{"Test"});
@@ -115,77 +140,55 @@ public class BuiltinAmputeTest extends AutomatedTestBase {
 	}
 
     @Test
-    public void testAmputeMCARstd() {
+    public void testAmputeMCAR() {
+        Double prop = defaultProp;
         boolean standardized = true;
         boolean continous = true;
-		runAmputeTest(MissingnessMech.MCAR,  standardized, continous, ExecMode.SINGLE_NODE, false);
+        boolean byCases = true;
+		runAmputeTest(prop, MissingnessMech.MCAR, standardized, continous, byCases, ExecMode.SINGLE_NODE, false);
+		runAmputeTest(prop, MissingnessMech.MCAR, false, continous, byCases, ExecMode.SINGLE_NODE, false);
+		runAmputeTest(prop, MissingnessMech.MCAR, standardized, false, byCases, ExecMode.SINGLE_NODE, false);
+        // prop has to be lowered for cellwise missingness proportion, 
+        // because of limited amount of zeros in patterns
+        // is checked in dml script and would trigger stop() call
+        prop /= numFeatures;
+		runAmputeTest(prop, MissingnessMech.MCAR, standardized, continous, false, ExecMode.SINGLE_NODE, false);
 	}
 
     @Test
-    public void testAmputeMCARnonStd() {
-        boolean standardized = false;
-        boolean continous = true;
-		runAmputeTest(MissingnessMech.MCAR,  standardized, continous, ExecMode.SINGLE_NODE, false);
-	}
-    
-    @Test
-	public void testAmputeMARstdDiscrete() {
-		boolean standardized = true;
-        boolean continous = false;
-		runAmputeTest(MissingnessMech.MAR,  standardized, continous, ExecMode.SINGLE_NODE, false);
-	}
-    
-    @Test
-    public void testAmputeMARnonStdDiscrete() {
-		boolean standardized = false;
-        boolean continous = false;
-		runAmputeTest(MissingnessMech.MAR,  standardized, continous, ExecMode.SINGLE_NODE, false);
-	}
-
-    @Test
-	public void testAmputeMNARstdDiscrete() {
-		boolean standardized = true;
-        boolean continous = false;
-		runAmputeTest(MissingnessMech.MNAR,  standardized, continous, ExecMode.SINGLE_NODE, false);
-	}
-    
-    @Test
-	public void testAmputeMNARnonStdDiscrete() {
-		boolean standardized = false;
-        boolean continous = false;
-		runAmputeTest(MissingnessMech.MNAR,  standardized, continous, ExecMode.SINGLE_NODE, false);
-	}
-    
-    @Test
-	public void testAmputeMARstdContinous() {
+	public void testAmputeMAR() {
+        Double prop = defaultProp;
 		boolean standardized = true;
         boolean continous = true;
-		runAmputeTest(MissingnessMech.MAR,  standardized, continous, ExecMode.SINGLE_NODE, false);
+        boolean byCases = true;
+		runAmputeTest(prop, MissingnessMech.MAR, standardized, continous, byCases, ExecMode.SINGLE_NODE, false);
+		runAmputeTest(prop, MissingnessMech.MAR, false, continous, byCases, ExecMode.SINGLE_NODE, false);
+		runAmputeTest(prop, MissingnessMech.MAR, standardized, false, byCases, ExecMode.SINGLE_NODE, false);
+        // prop has to be lowered for cellwise missingness proportion, 
+        // because of limited amount of zeros in patterns
+        // is checked in dml script and would trigger stop() call
+        prop /= numFeatures;
+		runAmputeTest(prop, MissingnessMech.MAR, standardized, continous, false, ExecMode.SINGLE_NODE, false);
 	}
     
     @Test
-	public void testAmputeMARnonStdContinous() {
-		boolean standardized = false;
-        boolean continous = true;
-		runAmputeTest(MissingnessMech.MAR,  standardized, continous, ExecMode.SINGLE_NODE, false);
-	}
-
-    @Test
-	public void testAmputeMNARstdContinous() {
+	public void testAmputeMNAR() {
+        Double prop = defaultProp;
 		boolean standardized = true;
         boolean continous = true;
-		runAmputeTest(MissingnessMech.MNAR,  standardized, continous, ExecMode.SINGLE_NODE, false);
+        boolean byCases = true;
+		runAmputeTest(prop, MissingnessMech.MNAR, standardized, continous, byCases, ExecMode.SINGLE_NODE, false);
+		runAmputeTest(prop, MissingnessMech.MNAR, false, continous, byCases, ExecMode.SINGLE_NODE, false);
+		runAmputeTest(prop, MissingnessMech.MNAR, standardized, false, byCases, ExecMode.SINGLE_NODE, false);
+        // prop has to be lowered for cellwise missingness proportion, 
+        // because of limited amount of zeros in patterns
+        // is checked in dml script and would trigger stop() call
+        prop /= numFeatures;
+		runAmputeTest(prop, MissingnessMech.MNAR, standardized, continous, false, ExecMode.SINGLE_NODE, false);
 	}
     
-    @Test
-	public void testAmputeMNARnonStdContinous() {
-		boolean standardized = false;
-        boolean continous = true;
-		runAmputeTest(MissingnessMech.MNAR,  standardized, continous, ExecMode.SINGLE_NODE, false);
-	}
 
-
-    private void runAmputeTest(MissingnessMech mech, boolean standardized, boolean continous, ExecMode mode, boolean lineage) {
+    private void runAmputeTest(Double prop, MissingnessMech mech, boolean standardized, boolean continous, boolean byCases, ExecMode mode, boolean lineage) {
         ExecMode execModeOld = setExecMode(mode);
         try {
             loadTestConfiguration(getTestConfiguration(TEST_NAME));
@@ -201,6 +204,8 @@ public class BuiltinAmputeTest extends AutomatedTestBase {
                 "weights=" + input("weights"),
                 "std=" + String.valueOf(standardized).toUpperCase(),
                 "cont=" + String.valueOf(continous).toUpperCase(),
+                "type=" + input("type"),
+                "byCases=" + String.valueOf(byCases).toUpperCase(),
                 "amputedData=" + output("amputedData"),
                 "nanMask=" + output("nanMask"),
                 "amputedRowCount=" + output("amputedRowCount"),
@@ -214,14 +219,26 @@ public class BuiltinAmputeTest extends AutomatedTestBase {
             writeInputMatrixWithMTD("patterns", patterns, false);
             writeInputMatrixWithMTD("freq", frequencies, false);
             writeInputMatrixWithMTD("weights", weights, false);
+            writeInputMatrixWithMTD("type", type, false);
 
-            runTest(true, EXCEPTION_NOT_EXPECTED, null, -1);
+            runTest(true, EXCEPTION_NOT_EXPECTED, null, -1); 
             MatrixCharacteristics mc = readDMLMetaDataFile("amputedData");
-            // System.out.print(readDMLScalarFromOutputDir("amputedRowCount").toString());
-            // System.out.print(readDMLScalarFromOutputDir("amputedCellCount").toString());
             Assert.assertEquals(numSamples, mc.getRows());
 			Assert.assertEquals(numFeatures, mc.getCols());
-
+            
+            Double sampleProp = 0.0;
+            if (byCases) {
+                Double amputedRowCount = readDMLScalarFromOutputDir("amputedRowCount").get(new CellIndex(1,1));
+                sampleProp = amputedRowCount / numSamples;
+            } else {
+                Double amputedCellCount = readDMLScalarFromOutputDir("amputedCellCount").get(new CellIndex(1,1));
+                sampleProp = amputedCellCount / (numSamples * numFeatures);
+            }
+            // works in most cases but then as expected when running too often there will be exceptions
+            // since a rather tight two sided alpha of 0.05 is used, but stays really close to expected prop
+            // commented out since its probably not a good idea to use it for automated tests
+            // Assert.assertTrue("proportion-test", proportionTest(prop, sampleProp));
+            
         } finally {
             resetExecMode(execModeOld);
         }
